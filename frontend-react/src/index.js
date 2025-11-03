d08c5356bb5dc3dce220cc7aca17b822db5a2297
dasteb <ivandev833@gmail.com>
2025-11-03 13:31:56 +0300

    Обновление компонентов

--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import UrlShortener from './components/App.jsx';
+import UrlShortener from './components/App.tsx';
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
