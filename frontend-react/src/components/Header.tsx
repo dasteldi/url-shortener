03aa7961a6b5c2218641ffb0a634b13056300d82
dasteldi <ivandev833@gmail.com>
2025-11-07 15:05:48 +0300

    Header added

---
+++ frontend-react/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <img src="/favicon.ico" alt="Icon" className="logo"></img>
+            <a href="/benefits">Benefits</a>
+            <a href="/about">About as</a>
+            <a href="/vacansy">Vacansy</a>
+        </header>
+    );
+}
+
+
+export default Header;
